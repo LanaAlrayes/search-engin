7257abaafae120427020560bfcc148716854e7f8
LanaAlrayes <lanaalrayss@gmail.com>
2022-09-22 23:53:24 +0300

    Add section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,32 @@
 <template>
-  <Search />
+  <div>
+    <hero />
+    <different-h />
+    <our-medicines />
+    <as-d />
+    <our-team />
+    <client-testimonials />
+  </div>
 </template>
 
 <script>
-import Search from '../components/Search'
+import Hero from '../components/Hero'
+import OurMedicines from '@/components/OurMedicines.vue'
+import OurTeam from '../components/OurTeam.vue'
+import DifferentH from '@/components/DifferentH.vue'
+import AsD from '@/components/AsD.vue'
+import ClientTestimonials from '@/components/ClientTestimonials.vue'
 
 export default {
   name: 'HomeView',
 
   components: {
-    Search,
+    Hero,
+    OurMedicines,
+    OurTeam,
+    DifferentH,
+    AsD,
+    ClientTestimonials,
   },
 }
 </script>
