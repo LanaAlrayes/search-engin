23a1212e358b5fb8787bae2feb506cdc0548c412
LanaAlrayes <lanaalrayss@gmail.com>
2022-09-30 11:26:50 +0300

    Add (different)

---
+++ src/components/DifferentV.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container
+    id="different"
+  >
+    <div
+      class="text-h3 mt-16 mb-8 black--text"
+    >
+      What Makes Us Different
+    </div>
+    <v-row class="mb-10">
+      <v-col
+        cols="6"
+        sm="4"
+      >
+        <v-avatar size="130">
+          <v-icon
+            large
+            color="light-blue darken-1"
+          >
+            mdi-seal
+          </v-icon>
+        </v-avatar>
+        <v-card-text class="mt-5">
+          <p class="text-h4 text--primary">
+            Origin
+          </p>
+          <p class="text-subtitle-1 grey--text">
+            We help to get the best medicine
+          </p>
+        </v-card-text>
+      </v-col>
+
+      <v-col
+        cols="6"
+        sm="4"
+      >
+        <v-avatar size="130">
+          <v-icon
+            large
+            color="light-blue darken-1"
+          >
+            mdi-seal-variant
+          </v-icon>
+        </v-avatar>
+        <v-card-text class="mt-5">
+          <p class="text-h4 text--primary">
+            Origin
+          </p>
+          <p class="text-subtitle-1 grey--text">
+            We Provide Essential Service For Your Health
+          </p>
+        </v-card-text>
+      </v-col>
+
+      <v-col
+        cols="6"
+        sm="4"
+      >
+        <v-avatar size="130">
+          <v-icon
+            large
+            color="light-blue darken-1"
+          >
+            mdi-email
+          </v-icon>
+        </v-avatar>
+        <v-card-text class="mt-5">
+          <p class="text-h4 text--primary">
+            Origin
+          </p>
+          <p class="text-subtitle-1 grey--text">
+            We Provide Essential Service For Your Health
+          </p>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.v-avatar{
+    border: 2px solid #EEEEEE;
+        top: 24px;
+        z-index: 1;
+}
+</style>
