35bc0e867775ad595c37b75ae6bff258585a29ae
LanaAlrayes <lanaalrayss@gmail.com>
2022-10-05 00:10:01 +0300

    add file(company)

---
+++ src/components/CompanyV.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-carousel
+    id="company"
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+    class="my-16"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-img
+        src="https://sigmadigitalpartners.com/themes/templatemonster/html/pharmia/assets/images/about/3.jpg"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <v-btn class="text-h6 ">
+            {{ slide }} Slide
+          </v-btn>
+        </v-row>
+      </v-img>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+      ],
+    }
+  },
+}
+</script>
