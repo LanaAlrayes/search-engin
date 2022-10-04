0f3bf3a7bb796ec91b57fb83836e006032b69432
LanaAlrayes <lanaalrayss@gmail.com>
2022-10-05 00:10:45 +0300

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,31 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView,
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: () => import(/* webpackChunkName: "team" */ '../views/TeamView.vue'),
+  },
+  {
+    path: '/company',
+    name: 'Companies',
+    component: () => import(/* webpackChunkName: "company" */ '../views/CompanyView.vue'),
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/ContactView.vue'),
   },
 ]
 
