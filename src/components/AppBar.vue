9642fc5ad054e208aebe433f3e8f6d52fe86a3dd
LanaAlrayes <lanaalrayss@gmail.com>
2022-10-14 07:50:30 +0300

    add theme & icon

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -41,9 +41,15 @@
         </v-tab>
       </v-tabs>
 
-      <v-btn icon>
-        <v-icon color="white">
-          mdi-magnify
+      <v-btn
+        icon
+        @click="$vuetify.theme.dark=!$vuetify.theme.dark"
+      >
+        <v-icon v-if="!$vuetify.theme.dark">
+          mdi-weather-night
+        </v-icon>
+        <v-icon v-else>
+          mdi-white-balance-sunny
         </v-icon>
       </v-btn>
       <v-btn icon>
@@ -65,7 +71,9 @@
 export default {
   name: 'App',
   data: () => ({
-    //
+    return: {
+      show: false,
+    },
   }),
 }
 </script>
