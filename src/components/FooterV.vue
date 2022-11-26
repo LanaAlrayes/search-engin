1459452e6e89044b92d2b074d28855d23de30bba
LanaAlrayes <lanaalrayss@gmail.com>
2022-11-26 08:13:11 +0200

    add logo,icons. delete button

--- src/components/FooterV.vue
+++ src/components/FooterV.vue
@@ -2,33 +2,98 @@
   <v-footer
     cols="12"
     md="3"
-    dark
     padless
-    height="450"
-    class="light-blue lighten-1 d-flex align-end"
+    height="350"
+    color="#2a93c9"
   >
     <v-container>
       <v-row
-        class="align-center justify-space-around"
+        class="align-center justify-space-between"
       >
+        <v-col
+          v-vue-aos="{animationClass:'animate__animated animate__fadeInRight'}"
+          cols="12"
+          md="4"
+        >
+          <v-img
+            :src="require(`../assets/logo3.png`)"
+            max-width="200"
+            max-height="150"
+          />
+          <div>
+            <v-btn
+              class="mx-1"
+              fab
+              small
+              color="white"
+            >
+              <v-icon
+                color="indigo"
+              >
+                {{ icons.facebook }}
+              </v-icon>
+            </v-btn>
+
+            <v-btn
+              class="mx-1"
+              fab
+              small
+              color="white"
+            >
+              <v-icon
+                color="cyan"
+              >
+                {{ icons.twitter }}
+              </v-icon>
+            </v-btn>
+            <v-btn
+              class="mx-1"
+              fab
+              small
+              color="white"
+            >
+              <v-icon
+                color="red"
+              >
+                {{ icons.google }}
+              </v-icon>
+            </v-btn>
+            <v-btn
+              class="mx-1"
+              fab
+              small
+              color="white"
+            >
+              <v-icon
+                color="purple"
+              >
+                {{ icons.instagra }}
+              </v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+
         <v-col
           cols="12"
           md="4"
-          class="pa-0"
+          class="mt-8"
         >
           <v-list
             v-vue-aos="{animationClass:'animate__animated animate__fadeInRight'}"
-            class="light-blue lighten-1"
+            color="#2a93c9"
           >
-            <v-subheader class="text-uppercase font-weight-bold text-h6">
+            <v-subheader class="text-uppercase font-weight-bold text-h6 white--text">
               Contact us
             </v-subheader>
+            <v-divider
+              class="mr-16"
+              color="white"
+            />
+
             <v-list-item>
-              <v-list-item-icon>
-                <v-icon color="#757575">
-                  mdi-phone
-                </v-icon>
-              </v-list-item-icon>
+              <v-icon color="#f1f7fc">
+                mdi-phone
+              </v-icon>
               <v-list-item>
                 <a
                   href="tel:#"
@@ -38,27 +103,12 @@
                 </a>
               </v-list-item>
             </v-list-item>
+
             <v-list-item>
-              <v-list-item-icon>
-                <v-icon color="#757575">
-                  mdi-email
-                </v-icon>
-              </v-list-item-icon>
-              <v-list-item>
-                <a
-                  class="link-aemail white--text text-decoration-none "
-                  href="mailto:lanaalrayss@gmail.com"
-                >
-                  {{ settings.search_email }}
-                </a>
-              </v-list-item>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon color="#757575">
-                  mdi-map-marker
-                </v-icon>
-              </v-list-item-icon>
+              <v-icon color="#f1f7fc">
+                mdi-map-marker
+              </v-icon>
+
               <v-list-item-content>
                 <v-list-item>
                   <a
@@ -70,32 +120,19 @@
                 </v-list-item>
               </v-list-item-content>
             </v-list-item>
-          </v-list>
-        </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-          class="pa-0"
-        >
-          <v-list
-            v-vue-aos="{animationClass:'animate__animated animate__fadeInUp'}"
-            class="light-blue lighten-1"
-          >
-            <v-subheader class="text-uppercase font-weight-bold text-h6">
-              Copular news
-            </v-subheader>
             <v-list-item>
-              Pills or Drink: What to  Choose and Why
-            </v-list-item>
-            <v-list-item style="color:#757575">
-              May 04,2021
-            </v-list-item>
-            <v-list-item>
-              Our Guide for Patients on a Budget
-            </v-list-item>
-            <v-list-item style="color:#757575">
-              May 04,2021
+              <v-icon color="#f1f7fc">
+                mdi-email
+              </v-icon>
+              <v-list-item>
+                <a
+                  class="link-aemail white--text text-decoration-none"
+                  href="mailto:lanaalrayss@gmail.com"
+                >
+                  {{ settings.search_email }}
+                </a>
+              </v-list-item>
             </v-list-item>
           </v-list>
         </v-col>
@@ -103,188 +140,142 @@
         <v-col
           cols="12"
           md="4"
-          class="pa-0"
+          class="mt-3"
         >
           <v-list
             v-vue-aos="{animationClass:'animate__animated animate__fadeInLeft'}"
-            class="light-blue lighten-1"
+            color="#2a93c9"
           >
-            <v-subheader class="text-uppercase font-weight-bold text-h6">
-              quick links
+            <v-subheader class="text-uppercase font-weight-bold text-h6 white--text">
+              NEWSLETTER
             </v-subheader>
+            <v-divider
+              class="mr-4"
+              color="white"
+            />
             <v-row>
               <v-col>
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#home')"
                     >
                       Home</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
 
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#different')"
                     >
                       Different</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
 
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#medicine')"
                     >
                       Counter Medicine</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
-                <v-hover v-slot="{hover}">
-                  <v-btn
-                    class="text-uppercase white--text font-weight-bold"
-                    :outlined="!hover"
-                    color="#757575"
-                    :to="{name: ''}"
-                  >
-                    search now
-                  </v-btn>
-                </v-hover>
               </v-col>
               <v-col>
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#company')"
                     >
                       Companies</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
 
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#ourTeam')"
                     >
                       Our Team</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
 
                 <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="#757575">
-                      mdi-circle-medium
-                    </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>
+                  <v-icon color="#f1f7fc">
+                    {{ icons.circle }}
+                  </v-icon>
+
+                  <v-list-item>
                     <a
                       class="white--text text-decoration-none "
                       @click="$vuetify.goTo('#client')"
                     >Client</a>
-                  </v-list-item-title>
+                  </v-list-item>
                 </v-list-item>
-                <v-hover v-slot="{hover}">
-                  <v-btn
-                    class="text-uppercase white--text font-weight-bold"
-                    :outlined="!hover"
-                    color="#757575"
-                    :to="{name: ''}"
-                  >
-                    get in touch
-                  </v-btn>
-                </v-hover>
               </v-col>
             </v-row>
           </v-list>
         </v-col>
       </v-row>
-    </v-container>
-    <v-card
-      flat
-      tile
-      class="blue-grey lighten-3 white--text "
-      width="100%"
-      min-height="20%"
-    >
-      <v-row
-        class="text-center pt-8"
+      <v-card
+        flat
+        tile
+        class="white--text"
+        width="100%"
+        min-height="20%"
+        color="#2a93c9"
       >
-        <v-col
-          cols="12"
-          md="3"
+        <div
+          class="pt-8"
         >
-          &copy; {{ new Date().getFullYear() }} — <strong>{{ settings.search_name }}. All rights reserved</strong>
-        </v-col>
-        <v-col>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-
-            icon
-            cols="12"
-            md="3"
-          >
-            <v-icon size="18px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <strong>privacy policy</strong>
-        </v-col>
-      </v-row>
-    </v-card>
+          &copy; {{ new Date().getFullYear() }} — <strong>{{ settings.search_name }} .Privacy Policy</strong>
+        </div>
+      </v-card>
+    </v-container>
   </v-footer>
 </template>
 <script>
 export default {
   name: 'App',
   data: () => ({
-    icons: [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-google',
-      'mdi-instagram',
-    ],
+    icons: {
+      facebook: 'mdi-facebook',
+      twitter: 'mdi-twitter',
+      google: 'mdi-google-plus',
+      instagra: 'mdi-instagram',
+      circle: 'mdi-circle-medium',
+    },
     settings: {
-      search_name: 'Search',
-      search_phone: '0123456789',
-      search_address: 'Damascus',
+      search_name: 'Syrian Drug Index',
+      search_phone: '+9963 954763265',
+      search_address: 'Syria Damascus',
       search_email: 'info@info.com',
-      search_logo: 'search.png',
     },
   }),
 
