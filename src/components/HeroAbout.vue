69f0b16a41f7b689d2d904742b81d4e8ea1d13ec
LanaAlrayes <lanaalrayss@gmail.com>
2022-11-26 08:15:56 +0200

    add hero(about and contact)

---
+++ src/components/HeroAbout.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-parallax
+    height="450"
+    src="https://livedemo00.template-help.com/wt_prod-20844/images/breadcrumbs-bg.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="text-h4 font-weight-bold">
+          About us
+        </h1>
+        <div class="colorBreads d-flex justify-center">
+          <v-breadcrumbs
+            :items="items"
+            large
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          text: 'Home',
+          disabled: false,
+          href: '/',
+        },
+        {
+          text: 'AboutUs',
+          disabled: true,
+          href: '/about',
+        }],
+    }
+  },
+}
+</script>
---
+++ src/components/HeroContact.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-parallax
+    height="450"
+    src="https://livedemo00.template-help.com/wt_prod-20844/images/breadcrumbs-bg.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="text-h4 font-weight-bold">
+          Contact us
+        </h1>
+        <div class="colorBreads d-flex justify-center">
+          <v-breadcrumbs
+            large
+            :items="items"
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          text: 'Home',
+          disabled: false,
+          href: '/',
+        },
+        {
+          text: 'ContactUs',
+          disabled: true,
+          href: '/contact',
+        }],
+    }
+  },
+}
+</script>
