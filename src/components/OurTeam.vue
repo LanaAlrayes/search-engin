76caf81fa6de47c2703322ff8e0870d34cf20b17
LanaAlrayes <lanaalrayss@gmail.com>
2022-12-01 21:23:25 +0200

    add parallax

--- src/components/OurTeam.vue
+++ src/components/OurTeam.vue
@@ -1,63 +1,112 @@
 <template>
-  <v-container id="ourTeam">
-    <v-row
-      class="my-16"
-      justify="center"
-    >
-      <v-col cols="12">
-        <div
-          v-vue-aos="{animationClass:'animate__animated animate__fadeInDown'}"
-          class="text-h6 text-center my-5 grey--text"
-        >
-          Meet Our Team
-        </div>
-        <div
-          v-vue-aos="{animationClass:'animate__animated animate__fadeInRight'}"
-          class="text-h3 text-center my-5"
-        >
-          Our Creative Team
-        </div>
-      </v-col>
-      <v-col
-        v-for="(teams,i) in team"
-        :key="i"
-        v-vue-aos="{animationClass:'animate__animated animate__fadeInLeft'}"
-        class="text-center justify-center ml-10"
-        cols="12"
-        md="3"
-        sm="4"
+  <v-parallax
+    height="100%"
+    src="https://demo.themexbd.com/rtl/smartdev/wp-content/uploads/2021/06/choose-bg.png"
+  >
+    <v-container id="ourTeam">
+      <v-row
+        class="my-16"
+        justify="center"
       >
-        <v-hover v-slot="{ hover }">
-          <div>
-            <v-avatar
-              size="170"
-              :class="{'border': hover}"
-            >
-              <img
-                alt="user"
-                :src="require(`../assets/${teams.img}`)"
-                class="d-flex justify-center"
-              >
-            </v-avatar>
+        <v-col
+          cols="12"
+        >
+          <div
+            v-vue-aos="{animationClass:'animate__animated animate__fadeInDown'}"
+            class="text-h6 text-center my-5 grey--text"
+          >
+            Meet Our Team
+          </div>
+          <div
+            v-vue-aos="{animationClass:'animate__animated animate__fadeInRight'}"
+            class="text-h3 text-center my-5 text--primary"
+          >
+            Our Creative Team
+          </div>
+        </v-col>
+
+        <v-col
+          v-for="(teams,i) in team"
+          :key="i"
+          v-vue-aos="{animationClass:'animate__animated animate__fadeInLeft'}"
+          class="text-center justify-center ml-10"
+          cols="12"
+          md="3"
+          sm="4"
+        >
+          <v-hover v-slot="{ hover }">
             <v-card
+              width="300"
+              height="350"
               class="mx-auto text-center justify-center"
             >
-              <!-- grey lighten-4 -->
-              <v-card-title class="justify-center pt-5">
+              <div>
+                <v-img
+                  alt="user"
+                  :src="require(`../assets/${teams.img}`)"
+                  height="270"
+                >
+                  <v-expand-x-transition>
+                    <div
+                      v-if="hover"
+                      class="blue"
+                      style="height:100%;
+                      width:20%;"
+                    >
+                      <div>
+                        <v-btn
+                          class="mx-1 mt-10"
+                          fab
+                          small
+                          color="white"
+                        >
+                          <v-icon
+                            color="indigo"
+                          >
+                            {{ icons.facebook }}
+                          </v-icon>
+                        </v-btn>
+                        <v-btn
+                          class="mx-1 mt-5"
+                          fab
+                          small
+                          color="white"
+                        >
+                          <v-icon
+                            color="indigo"
+                          >
+                            {{ icons.google }}
+                          </v-icon>
+                        </v-btn>
+                        <v-btn
+                          class="mx-1 mt-5"
+                          fab
+                          small
+                          color="white"
+                        >
+                          <v-icon
+                            color="indigo"
+                          >
+                            {{ icons.linked }}
+                          </v-icon>
+                        </v-btn>
+                      </div>
+                    </div>
+                  </v-expand-x-transition>
+                </v-img>
+              </div>
+              <v-card-title class="justify-center pt-2">
                 {{ teams.name }}
               </v-card-title>
               <v-card-subtitle class="blue--text text--darken-1">
                 {{ teams.job_title }}
               </v-card-subtitle>
-              <v-card-text>
-                Syrian Virtual University
-              </v-card-text>
             </v-card>
-          </div>
-        </v-hover>
-      </v-col>
-    </v-row>
-  </v-container>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-parallax>
 </template>
 
 <script>
@@ -69,26 +118,34 @@ export default {
           name: 'Lana Alrayes',
           job_title: 'Front End',
           img: 'Lana.jpg',
+          university: 'Syrian Virtual University',
         },
         {
           name: 'Ammar Khadaj',
           job_title: 'Data Bases',
           img: 'Amar.jpg',
+          university: 'Syrian Virtual University',
         },
         {
           name: 'Reem Aboras',
           job_title: 'Back End',
-          img: 'Lana.jpg',
+          img: 'Reem.png',
+          university: 'Syrian Virtual University',
         },
 
       ],
+      icons: {
+        facebook: 'mdi-facebook',
+        linked: 'mdi-linkedin',
+        google: 'mdi-google-plus',
+      },
     }
   },
 }
 </script>
 <style scoped>
 .border{
-    position: relative;
+  position: relative;
     content: '';
     top: 24px;
     right: 0;
