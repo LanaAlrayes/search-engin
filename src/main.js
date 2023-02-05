7165668636bfb6e14258e2a11cfbfd19ff35c890
LanaAlrayes <lanaalrayss@gmail.com>
2023-02-05 14:33:52 +0200

    add axios and vee-validate

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
-import VueAos from 'vue-aos'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import * as VueAos from 'vue-aos'
+import { ValidationProvider } from 'vee-validate'
 import 'animate.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 Vue.use(VueAos)
+Vue.component('ValidationProvider', ValidationProvider)
 
 new Vue({
   router,
