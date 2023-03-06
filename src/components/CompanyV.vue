36274254d78240dcd12f9944a24ba8ac068db4ab
LanaAlrayes <lanaalrayss@gmail.com>
2023-03-06 09:17:16 +0200

    add button

--- src/components/CompanyV.vue
+++ src/components/CompanyV.vue
@@ -1,41 +1,47 @@
 <template>
-  <v-hover>
-    <template #default="{ hover }">
-      <v-carousel
-        id="company"
-        cycle
-        height="500"
-        hide-delimiter-background
-        show-arrows-on-hover
-      >
-        <v-carousel-item
-          v-for="(item, i) in items"
-          :key="i"
-          :src="item.src"
-        />
-        <v-fade-transition>
-          <v-overlay
-            v-if="hover"
-            absolute
-            color="#90CAF9"
-          >
+  <v-carousel
+    id="company"
+    cycle
+    height="500"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(photo, i) in photos"
+      :key="i"
+      :src="photo.src"
+    >
+      <v-overlay absolute>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-container class="justify-center text-center">
+            <div
+              v-vue-aos="{animationClass:'animate__animated animate__slideInUp'}"
+              class="justify-center text-center text-h4"
+            >
+              Welcome to our company
+            </div>
             <v-btn
+              v-vue-aos="{animationClass:'animate__slideInDown animate__animated'}"
+              class="mt-6"
               :to="{name: 'Companies'}"
             >
               See more info
             </v-btn>
-          </v-overlay>
-        </v-fade-transition>
-      </v-carousel>
-    </template>
-  </v-hover>
+          </v-container>
+        </v-row>
+      </v-overlay>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      items: [
+      photos: [
         {
           src: 'https://livedemo00.template-help.com/wt_prod-20844/images/project-3-671x214.jpg',
         },
@@ -46,7 +52,6 @@ export default {
           src: 'https://ld-wt73.template-help.com/wt_prod-20176/images/background-breadcrumbs-01-1920x345.jpg',
         },
       ],
-      overlay: false,
     }
   },
 }
