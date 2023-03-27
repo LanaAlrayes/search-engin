735f38da44e8add877c1d9d5e274e24e266363df
LanaAlrayes <lanaalrayss@gmail.com>
2023-03-27 13:13:39 +0200

    Add login methods

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <!-- <v-parallax
-    height="100%"
-    src="https://demo.themexbd.com/rtl/smartdev/wp-content/uploads/2021/06/choose-bg.png"
-  > -->
     <svg
       class="mt-12"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 224"
     ><path
       fill="#2a93c9"
-      fill-opacity="0.66"
+      fill-opacity="0.70"
       d="M0,32L40,32C80,32,160,32,240,64C320,96,400,160,480,154.7C560,149,640,75,720,80C800,85,880,171,960,192C1040,213,1120,171,1200,165.3C1280,160,1360,192,1400,208L1440,224L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
     /></svg>
     <v-container>
@@ -39,7 +35,6 @@
               </v-list-item-content>
             </v-list-item>
             <validation-observer
-              ref="observer"
               v-slot="{ invalid }"
             >
               <form
@@ -48,92 +43,89 @@
               >
                 <validation-provider
                   v-slot="{ errors }"
-                  name="phoneNumber"
-                  :rules="{
-                    required: true,
-                    regex: '^(\\+9639)'
-                  }"
+                  name="userName"
+                  rules="required"
                 >
                   <v-text-field
-                    v-model="phoneNumber"
+                    v-model="userName"
+                    color="#000000"
+                    class="mx-10 text-dark"
+                    :counter="50"
+                    :error-messages="errors"
+                    label="Name"
+                    required
+                    outlined
+                  />
+                </validation-provider>
+
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="password"
+                  rules="required"
+                >
+                  <v-text-field
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show1 ? 'text' : 'password'"
                     color="#000000"
                     class="mx-10 text-dark"
                     :counter="20"
                     :error-messages="errors"
-                    label="Phone Number"
+                    label="Password"
                     required
                     outlined
+                    @click:append="show1 = !show1"
                   />
-
-                  <validation-provider
-                    v-slot="{errors }"
-                    name="password"
-                    rules="required"
-                  >
-                    <v-text-field
-                      v-model="password"
-                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :type="show1 ? 'text' : 'password'"
-                      color="#000000"
-                      class="mx-10 text-dark"
-                      :error-messages="errors"
-                      label="Password"
-                      required
-                      outlined
-                      @click:append="show1 = !show1"
-                    />
-                  </validation-provider>
-
-                  <v-btn
-                    class="ml-12"
-                    type="submit"
-                    color="success"
-                    :disabled="invalid"
-                  >
-                    LogIn
-                  </v-btn>
-
-                  <v-dialog
-                    v-model="dialog"
-                    width="500"
-                  >
-                    <v-card>
-                      <v-card-title
-                        class="text-h5 amber lighten-2"
-                      >
-                        Wrong login
-                      </v-card-title>
-                      <v-card-text class="text-center mt-5">
-                        Please try again
-                      </v-card-text>
-                      <v-divider />
-                      <v-card-actions>
-                        <v-spacer />
-                        <v-btn
-                          color="success"
-                          @click="dialog = false"
-                        >
-                          OK
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-
-                  <v-btn
-                    class="ml-8"
-                    color="success"
-                    @click="clear"
-                  >
-                    clear
-                  </v-btn>
                 </validation-provider>
+
+                <v-btn
+                  class="ml-12"
+                  type="submit"
+                  color="success"
+                  :disabled="invalid"
+                >
+                  LogIn
+                </v-btn>
+
+                <v-dialog
+                  v-model="dialog"
+                  width="500"
+                >
+                  <v-card>
+                    <v-card-title
+                      class="text-h6 success"
+                    >
+                      Wrong login
+                    </v-card-title>
+                    <v-card-text class="text-center text-h6 mt-5">
+                      Please try again
+                    </v-card-text>
+                    <v-divider />
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn
+                        color="success"
+                        @click="dialog = false"
+                      >
+                        OK
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+                <v-btn
+                  class="ml-8"
+                  color="success"
+                  @click="clear"
+                >
+                  clear
+                </v-btn>
               </form>
             </validation-observer>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
-  <!-- </v-parallax> -->
   </div>
 </template>
 <script>
@@ -160,7 +152,7 @@ export default {
 
   data: () => ({
     show1: false,
-    phoneNumber: '',
+    userName: '',
     password: '',
     dialog: '',
 
@@ -168,17 +160,36 @@ export default {
 
   methods: {
     submit () {
-      this.$refs.observer.validate()
+      const payload = {
+        user_name: this.userName,
+        password: this.password,
+
+      }
+      const self = this
+
+      this.axios.post('/login_user/', payload).then(res => {
+        // console.log(res.data)
+        if (res.data === 'True') {
+          // alert('welcom')
+          localStorage.setItem('user_token', res.data.token)
+          self.$store.state.token = res.data.token
+          window.location = '/'
+        } else {
+          this.dialog = true
+        }
+        this.$refs.observer.validate()
+      })
     },
+
     clear () {
-      this.phoneNumber = ''
+      this.userName = ''
       this.password = ''
       this.$refs.observer.reset()
     },
   },
 }
 </script>
-<style>
+<style scoped>
 .background{
   position: relative;
     transform: translate(-50%,-50%);
@@ -187,7 +198,6 @@ export default {
     opacity: 0.9;
     width:400px;
     height:500px;
-
 }
 
 </style>
